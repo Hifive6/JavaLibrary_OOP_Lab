f1a5648a933967f61bfa7b73690588a873bc3adf
David Juarez <djuar05@gmail.com>
2020-05-20 14:10:59 -0400

    create periodical class extending Library item class

---
+++ Periodical.java
@@ -0,0 +1,7 @@
+public class Periodical extends LibraryItem {
+    
+ public Periodical(String title, String author, String publisher, int publishYear, int ISBN){
+     super(title, author, publisher, publishYear, ISBN);
+
+ }   
+}
