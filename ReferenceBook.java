c635d2f90868244a49d3ab11f91f6f895e91e162
David Juarez <djuar05@gmail.com>
2020-05-20 14:27:53 -0400

    create referencebook extending Book class, create private fields of type of reference and edition

---
+++ ReferenceBook.java
@@ -0,0 +1,29 @@
+public class ReferenceBook extends Book{
+    
+    private String infoType;
+    private int edition;
+    
+    public ReferenceBook(String title, String author, String publisher, int publishYear, int ISBN, String infoType, int edition){
+        super(title, author, publisher, publishYear, ISBN);
+        this.infoType = infoType;
+        this.edition = edition;
+    }
+
+    //getters for Reference Book
+    public String getInfoType(){
+        return infoType;
+    }
+
+    public int getEdition(){
+        return edition;
+    }
+
+    //settlers for Reference Book
+    public void setInfoType(String infotype){
+        this.infoType = infotype;
+    }
+
+    public void setEdition(int edition){
+        this.edition = edition;
+    }
+}
