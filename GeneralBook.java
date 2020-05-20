d7ca1b5a2cea7d90d146d6688409f54439c44b22
David Juarez <djuar05@gmail.com>
2020-05-20 16:24:09 -0400

    create General book extending Book adding genre and fiction and an override to check if general book is fiction or non fiction

---
+++ GeneralBook.java
@@ -0,0 +1,41 @@
+public class GeneralBook extends Book {
+    
+    private String genre;
+    private boolean fiction;
+    
+
+    public GeneralBook(String title, String author, String publisher, int publishYear, int ISBN, String genre, boolean fiction){
+        super(title, author, publisher, publishYear, ISBN);
+        this.genre = genre;
+        this.fiction = fiction;
+    }
+
+    // getters
+    public String getGenre(){
+        return genre;
+    }
+
+    public boolean getFiction(){
+        return fiction;
+    }
+
+    //setters
+    public void setGenre(String genre){
+        this.genre = genre;
+    }
+
+    public void setFiction(boolean fiction){
+        this.fiction = fiction;
+    }
+
+    @Override
+    public String toString(){
+        String str = super.toString();
+
+        if(fiction){
+           return str += ", Fiction";
+        } else {
+            return str += ", Non-Fiction";
+        }
+    }
+}
